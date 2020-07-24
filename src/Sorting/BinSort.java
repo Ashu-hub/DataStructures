af1f4b78b1967f4c83a864e0153f6b5e61be39ed
Ashutosh ranjan <ashutoshranjan82@gmail.com>
2020-07-24 13:59:52 +0530

    Update BinSort.java

--- src/Sorting/BinSort.java
+++ src/Sorting/BinSort.java
@@ -4,6 +4,12 @@ public class BinSort {
 
 	public static void main(String[] args) {
 		int [] a = {6, 3, 9, 10, 15, 6, 8, 12, 3, 6};
+		BSort(a,a.length);
+	}
+
+	private static void BSort(int[] a, int length) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
